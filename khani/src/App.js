3aa260531506e38caa828fffea30adb7da234a0f
khanimozhi <106865965+khanimozhi@users.noreply.github.com>
2022-11-21 11:54:20 +0530

    Add files via upload

---
+++ khani/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./App.css";
+import Appe from './home';
+
+function App() {
+  return (
+   <>
+   <div className='container'>
+    <div className='row'>
+      <Appe />
+    </div>
+
+   </div>
+   </>
+  );
+}
+
+export default App;
---
+++ khani/src/home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './App.css'
+
+function Appe() {
+  return (
+    <>
+<nav class="nav-main">
+  <ul>
+    <li><a href="#">Products</a></li>
+    <li><a href="#">Services</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</nav>
+<header>
+  <h1>BEAUTY PARLOUR</h1>
+</header>
+<article>
+  <h1 id="hoses">HAIR SPA</h1>
+  <p>A typical hair spa consists of oil massaging, shampoo, hair mask, and conditioning. Together, these steps help recover the hair's shine and moisture, which is often lost due to pollution and dryness. A hair spa treatment's primary function is to improve your hair's texture and overall shine</p>
+<h1 id="rakes">MAKE UP</h1>
+<p>If beauty is what you live and breathe, you already know the popular faces on Instagram. Can you imagine a day without scrolling through their feeds and not knowing what’s new and trending in the beauty biz? But, what makes for a follow-worthy blogger? Someone who advocates beauty but not the beauty standards and someone who is real and relatable. Here, we speak to four content creators and beauty bloggers on their beauty philosophies and what sets them apart.</p>
+<h1 id="shovels">PEDICURE</h1>
+<p> A simple treatment that includes foot soaking, foot scrubbing with a pumice stone or foot file, nail clipping, nail shaping, foot and calf massage, moisturizer and nail polishing.</p>
+</article>
+<button>Read More</button>
+</>
+ );
+}
+
+export default Appe;
